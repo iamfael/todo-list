100618f75757d4ce5a394381fc239053eacb4607
Rafael Martins <martinscivil72@gmail.com>
2023-12-15 03:21:01 -0300

    /2
    function renderTodoList

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const todoList = [];
+const todoList = ["caminhada", "fazer comida"];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}<p>`;
+    todoListHTML += html;
+  }
+
+  console.log(todoListHTML);
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
@@ -6,4 +21,7 @@ function addTodo() {
 
   todoList.push(name);
   console.log(todoList);
+
+  inputElement.value = "";
+  renderTodoList();
 }
